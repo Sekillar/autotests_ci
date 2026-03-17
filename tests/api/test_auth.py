b3ab61ce8ee603ef6181735d6f87d5444f8eabc2
Sekillar <olgasilanova@gmail.com>
2026-03-17 20:27:44 +0300

    удалила лишний файл с тестами

--- tests/api/test_auth.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def test_status():
-    resp = requests.get("https://b2b.firstline-dev.com/")
-    assert resp.status_code == 200, f"Статус код некорректный, \n{resp.status_code}"
-    print("API работает")
